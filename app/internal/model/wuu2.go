0783db3d8834c4c1cfdb62878c7ace313a748e8b
Al Wilde <al@wilde.io>
2026-03-09 03:11:17 +0000

    Added version info to model

--- app/internal/model/wuu2.go
+++ app/internal/model/wuu2.go
@@ -56,6 +56,7 @@ type Steam struct {
 }
 
 type Information struct {
+	Version         string `json:"version"`
 	TotalRequests   uint64 `json:"totalRequests"`
 	ServerStartTime string `json:"serverStartTime"`
 }
